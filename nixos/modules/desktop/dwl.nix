056d50c224bc7304421ea8621170f7e2df69c5d5
ARLJohnston <github@arljohnston.com>
2025-03-25 22:01:46 +0000

    Remove commented code from dwl

--- nixos/modules/desktop/dwl.nix
+++ nixos/modules/desktop/dwl.nix
@@ -4,8 +4,7 @@
   config,
   ...
 }: let
-  #dwlbar-cmd = "dwlb -font \"MonoLisa Nerd Font:size=10\"";
-  dwlbar-cmd = "somebar";
+  dwlbar-cmd = "dwlb -font \"MonoLisa Nerd Font:size=10\"";
 in {
   options = {
     dwl-desktop.enable =
@@ -41,19 +40,9 @@ in {
       (self: super: {
         dwl = super.dwl.overrideAttrs (oldAttrs: rec {
           configFile =
-            super.writeText "config.h"
+            super.writeText "config.def.h"
             (builtins.readFile (pkgs.substituteAll {src = ./config.h;}));
           patches = [
-            # (pkgs.fetchpatch {
-            #   url =
-            #     "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/bar/bar-0.6.patch";
-            #   hash = "sha256-lq1/1f5DMHKTplUzYix4XUGKDarKz7X/5LSjz53PDcc=";
-            # })
-            # (pkgs.fetchpatch {
-            #   url =
-            #     "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/bar/bar-0.7.patch";
-            #   hash = "sha256-WbT3wp3hIp6ixud9q27F+hn7zq/4be6ZUpGWtCWX9tw=";
-            # })
             (pkgs.fetchpatch {
               url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/autostart/autostart.patch";
               hash = "sha256-OTQ/0O62wG3OKCzA2FGyFpgbNup5Xmia1techndd4I8=";
